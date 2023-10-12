28f90c846b2e7db4887ef999580d6bc24835d5b5
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2023-10-12 11:20:35 +0000

    My soultion

---
+++ basic-exercises-part-i/Exercise1.java
@@ -0,0 +1,12 @@
+//Exercise 1. 
+//Write a Java program to print 'Hello' on screen and your name on a separate line.
+//Expected Output :
+//Hello
+//Alexandra Abramov
+
+public class Exercise1{
+    public static void main(String[] args){
+        System.out.println("Hello");
+        System.out.println("Alexandra Abramov");
+    }
+}
