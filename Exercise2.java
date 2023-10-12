95f671dbacbaee26457c9fe9887a10f10fe3b27c
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2023-10-12 11:24:53 +0000

    My solution

---
+++ Exercise2.java
@@ -0,0 +1,14 @@
+// Exercise 2.
+// Write a Java program to print the sum of two numbers.
+// Test Data:
+// 74 + 36
+// Expected Output :
+// 110
+
+public class Exercise2 {
+    public static void main(String[] args){
+        int num1 = 74;
+        int num2 = 36;
+        System.out.println(num1 + num2);
+    }
+}
