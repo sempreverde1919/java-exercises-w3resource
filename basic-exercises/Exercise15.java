a01c0dc432cb6c1d8f7c4c2353a0919d5611461b
sempreverde1919 <110019528+sempreverde1919@users.noreply.github.com>
2023-10-16 11:47:57 +0000

    My solution

---
+++ basic-exercises/Exercise15.java
@@ -0,0 +1,11 @@
+// Exercise 15.
+// Write a Java program to swap two variables.
+
+public class Exercise15 {
+    public static void main(String[] args) {
+        int a=1, b=2, temp=0;
+        a = temp;
+        a = b;
+        b = temp;
+    }
+}
